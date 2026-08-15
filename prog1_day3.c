e928b88a9fcf37b487234748ef85035640ecc26e
mahajant180-create <mahajant180@gmail.com>
2026-08-15 10:52:18 +0530

    Add files via upload

---
+++ prog1_day3.c
@@ -0,0 +1,28 @@
+Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
+#include <stdio.h>
+
+int main() {
+    int c, f;
+
+    scanf("%d", &c);
+
+    f = (c * 9 / 5) + 32;
+
+    printf("Fahrenheit=%d", f);
+
+    return 0;
+}
