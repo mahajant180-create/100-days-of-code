5b20879e909fa7f5a4127a8aa99744260b5e5b58
mahajant180-create <mahajant180@gmail.com>
2026-08-27 12:50:13 +0530

    Add files via upload

---
+++ prog2_day16.c
@@ -0,0 +1,37 @@
+\\Q32: Write a program to check if a number is a palindrome.
+
+/*
+Sample Test Cases:
+Input 1:
+121
+Output 1:
+Palindrome
+
+Input 2:
+123
+Output 2:
+Not palindrome
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n, temp, rev = 0, rem;
+
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (n != 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    if (temp == rev)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+
+    return 0;
+}
