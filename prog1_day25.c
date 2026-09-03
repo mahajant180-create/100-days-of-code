988a966e869f0e5e396d476eb591f710cd9c1b93
mahajant180-create <mahajant180@gmail.com>
2026-09-03 23:14:19 +0530

    Add files via upload

---
+++ prog1_day25.c
@@ -0,0 +1,38 @@
+//Q49: Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+5
+45
+345
+2345
+12345
+
+*/
+Q49: Write a program to print the following pattern:
+5
+45
+345
+2345
+12345
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+5
+45
+345
+2345
+12345
+
+*/
